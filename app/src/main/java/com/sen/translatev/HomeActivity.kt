147c7c6109630f72184ec50f21d9a5b96d6d2e7b
jsen17 <jsen17@163.com>
2020-08-10 17:06:33 +0800

    update 2020-08-10

--- app/src/main/java/com/sen/translatev/HomeActivity.kt
+++ app/src/main/java/com/sen/translatev/HomeActivity.kt
@@ -1,4 +1,34 @@
 package com.sen.translatev
 
-class HomeActivity  {
+import android.view.View
+import base.activity.BaseActivity
+import com.sen.translatev.databinding.ActHomeBinding
+import utils.go
+
+class HomeActivity  : BaseActivity<ActHomeBinding>(), View.OnClickListener {
+    override fun setLayoutId(): Int {
+        return R.layout.act_home
+    }
+
+
+    override fun initView() {
+
+        binding.apply {
+            createNew.setOnClickListener(this@HomeActivity)
+            eidtVideo.setOnClickListener(this@HomeActivity)
+//            test.setOnClickListener(this@MainActivity)
+        }
+    }
+
+    override fun onClick(v: View?) {
+        when (v?.id) {
+            R.id.createNew -> {
+//                go(this@HomeActivity, ImagePickerActivity::class.java)
+            }
+            R.id.eidtVideo -> {
+//                go(this@MainActivity, LocalVideoActivity::class.java)
+
+            }
+        }
+    }
 }
