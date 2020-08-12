74a938bd58dff7fe54cd4a5fef255bf1e7a15e0b
jsen17 <jsen17@163.com>
2020-08-12 16:23:01 +0800

    准备写功能

--- app/src/main/java/com/sen/translatev/HomeActivity.kt
+++ app/src/main/java/com/sen/translatev/HomeActivity.kt
@@ -1,11 +1,16 @@
 package com.sen.translatev
 
 import android.view.View
+import android.widget.Toast
 import base.activity.BaseActivity
+import com.cocosw.bottomsheet.BottomSheet
+import com.qmuiteam.qmui.widget.dialog.QMUIBottomSheet.BottomListSheetBuilder
 import com.sen.translatev.databinding.ActHomeBinding
-import utils.go
 
-class HomeActivity  : BaseActivity<ActHomeBinding>(), View.OnClickListener {
+
+class HomeActivity : BaseActivity<ActHomeBinding>(), View.OnClickListener {
+    val TAG_TAKE_PHOTO = 0
+    val TAG_LOCAL_IMAGE = 1
     override fun setLayoutId(): Int {
         return R.layout.act_home
     }
@@ -24,9 +29,20 @@ class HomeActivity  : BaseActivity<ActHomeBinding>(), View.OnClickListener {
         when (v?.id) {
             R.id.tranlateImage -> {
 //                go(this@HomeActivity, ImagePickerActivity::class.java)
+                BottomSheet.Builder(MainActivity@this, R.style.BottomSheet_StyleDialog).title("图片选择")
+                    .sheet(R.menu.dialog_menu)
+                    .listener { dialog, which ->
+                        when (which) {
+                            R.id.take_photo -> Toast.makeText(MainActivity@this, "拍照", Toast.LENGTH_SHORT)
+                                .show()
+                            R.id.loacl_image -> Toast.makeText(MainActivity@this, "本地图片", Toast.LENGTH_SHORT)
+                                .show()
+
+                        }
+                    }.show()
             }
             R.id.pdfOption -> {
-//                go(this@MainActivity, LocalVideoActivity::class.java)
+
 
             }
         }
